5caab2328c47402b2e23a201f761cc48aea75eb6
josephsdavid <josephsd@smu.edu>
2022-07-14 00:07:38 -0500

    fennel time?

--- init.lua
+++ init.lua
@@ -1,203 +1,28 @@
-require('impatient').enable_profile()
-require "plugins"
-require "globals"
-require "options"
-require "lsp".setup()
-require "ts"
-require "snippets"
-require "completion"
-require "term"
-require "bars"
-require "search"
-require "norg"
-require "neogen".setup()
-require "Comment".setup()
-require "gh"
-require "build"
-require "keybinds"
+-- Entrypoint for my Neovim configuration!
+-- We simply bootstrap packer and Aniseed here.
+-- It's then up to Aniseed to compile and load fnl/config/init.fnl
 
-require("neogen").setup({
-  snippet_engine = "luasnip",
-  enable_placeholders = false,
-  enabled = true,
-  languages = {
-    python = {
-      template = {
-        annotation_convention = "numpydoc",
-      },
-    },
-  },
-})
+local execute = vim.api.nvim_command
+local fn = vim.fn
 
-require("todo-comments").setup({
-  keywords = {
-    FIX = {
-      icon = " ", -- icon used for the sign, and in search results
-      color = "error", -- can be a hex color, or a named color (see below)
-      alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-      -- signs = false, -- configure signs for some keywords individually
-    },
-    TODO = { icon = " ", color = "info" },
-    HACK = { icon = " ", color = "warning" },
-    WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
-    PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-    NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-  },
+local pack_path = fn.stdpath("data") .. "/site/pack"
+local fmt = string.format
 
-  priority = 19,
-  highlight = {
-    before = "", -- "fg" or "bg" or empty
-    keyword = "wide", -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
-    after = "fg", -- "fg" or "bg" or empty
-    pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
-    comments_only = true, -- uses treesitter to match keywords in comments only
-    max_line_len = 400, -- ignore lines longer than this
-    exclude = {}, -- list of file types to exclude highlighting
-  },
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-})
-
-vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
-
-function scandir(directory)
-  local i, t, popen = 0, {}, io.popen
-  local pfile = popen('ls -a "' .. directory .. '"')
-  for filename in pfile:lines() do
-    i = i + 1
-    t[i] = filename
-  end
-  pfile:close()
-  return t
-end
-
-function find_string(s, t)
-  for k, v in pairs(t) do
-    if v == s
-    then
-      return true
-    end
-  end
-  return false
-end
-
-
-function F()
-  local result = vim.api.nvim_exec([[pwd]], true)
-  local out = scandir(result)
-  local ret = ""
-  if find_string(".JuliaFormatter.toml", out)
-  then
-    ret = "success!"
-  else
-    ret = "failure!!"
-  end
-  return ret
-end
-
-vim.cmd([[ command! FormatStyle execute 'lua F()' ]])
-
-
--- vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
-
-function _G.Toggle_venn()
-  local venn_enabled = vim.inspect(vim.b.venn_enabled)
-  if venn_enabled == "nil" then
-    vim.b.venn_enabled = true
-    vim.cmd [[setlocal ve=all]]
-    -- draw a line on HJKL keystokes
-    vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
-    vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
-    -- draw a box by pressing "f" with visual selection
-    vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
-  else
-    vim.cmd [[setlocal ve=]]
-    vim.cmd [[mapclear <buffer>]]
-    vim.b.venn_enabled = nil
+function ensure (user, repo)
+  -- Ensures a given github.com/USER/REPO is cloned in the pack/packer/start directory.
+  local install_path = fmt("%s/packer/start/%s", pack_path, repo, repo)
+  if fn.empty(fn.glob(install_path)) > 0 then
+    execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+    execute(fmt("packadd %s", repo))
   end
 end
 
-vim.api.nvim_set_keymap("n", "<leader>H", "", { noremap = true, callback = function() print("Hello world!") end, })
+-- Bootstrap essential plugins required for installing and loading the rest.
+ensure("wbthomason", "packer.nvim")
+ensure("Olical", "aniseed")
 
--- toggle keymappings for venn using <leader>v
-vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true })
-vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd InsertLeave,WinEnter * set cursorline
-    autocmd InsertEnter,WinLeave * set nocursorline
-    autocmd FileType qf set nobuflisted
-  augroup end
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-  "augroup _spelling
-  "  autocmd!
-  "  autocmd FileType julia setlocal spell
-  "augroup end
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal linebreak
-    autocmd FileType markdown setlocal spell
-    autocmd FileType norg setlocal wrap
-    autocmd FileType norg setlocal linebreak
-    " autocmd FileType norg setlocal spell
-  augroup end
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
-  " augroup ProjectDrawer
-  "     autocmd!
-  "     autocmd VimEnter * if argc() == 0 | Explore! | endif
-  " augroup END
-  " autocmd BufEnter * if expand("%:p:h") !~ '*.norg' | silent! tcd %:p:h | endif
-  let g:hiPairs_enable_matchParen = 0
-  let g:hiPairs_timeout = 1
-  let g:hiPairs_insert_timeout = 1
-  let g:hiPairs_hl_matchPair = { 'term'    : 'underline,bold',
-              \                  'cterm'   : 'underline,bold',
-              \                  'ctermfg' : '0',
-              \                  'ctermbg' : '180',
-              \                  'gui'     : 'underline,bold,italic',
-              \                  'guifg'   : '#fb94ff',
-              \                  'guibg'   : 'NONE' }
-autocmd FileType haskell setlocal expandtab shiftwidth=2 softtabstop=2
-autocmd FileType r setlocal expandtab shiftwidth=2 softtabstop=2
-autocmd FileType julia setlocal expandtab shiftwidth=4 softtabstop=4
-autocmd FileType rmd setlocal expandtab shiftwidth=2 softtabstop=2
-autocmd FileType yaml setlocal expandtab shiftwidth=2 softtabstop=2
-autocmd FileType lua setlocal expandtab shiftwidth=2 softtabstop=2
-" Run currently focused python script with F9
-" autocmd FileType python nnoremap <buffer> <F9> :exec '!python' shellescape(@%, 1)<cr>
-" autocmd FileType markdown nnoremap <buffer> <F9> :exec '!md2pdf' shellescape(@%, 1)<cr>
-" format code with f8
-autocmd FileType yaml nnoremap <buffer> <F8> :exec '!cfn-lint' shellescape(@%, 1)<cr>
-autocmd FileType python nnoremap <buffer> <F8> :exec '!yapf -i' shellescape(@%, 1)<cr>
-autocmd FileType rust nnoremap <buffer> <F8> :exec '!cargo fmt'<cr>
-autocmd FileType sh nnoremap <buffer> <F8> :exec '!shfmt -w' shellescape(@%, 1)<cr>
-autocmd FileType nix nnoremap <buffer> <F8> :exec '!nixfmt' shellescape(@%, 1)<cr>
-autocmd FileType lua nnoremap <buffer> <F8> :exec '!stylua' shellescape(@%, 1)<cr>
-filetype plugin on
-filetype indent on
-syntax enable
-fun! CleanExtraSpaces()
-	let save_cursor = getpos(".")
-	let old_query = getreg('/')
-	silent! %s/\s\+$//e
-	call setpos('.', save_cursor)
-	call setreg('/', old_query)
-endfun
-if has("autocmd")
-	autocmd BufWritePre *.txt,*.jl,*.js,*.py,*.wiki,*.sh,*.coffee,*.lua :call CleanExtraSpaces()
-endif
-]]
+-- Enable Aniseed's automatic compilation and loading of Fennel source code.
+vim.g["aniseed#env"] = {
+  module = "init",
+  compile = true
+}
