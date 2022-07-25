026ab2d6af05c98aeab408396eb6458423596120
josephsdavid <josephsd@smu.edu>
2022-07-25 10:24:05 -0500

    lsp go brr

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -291,10 +291,11 @@ M.setup = function()
     julials = {
 
     cmd = {
-
         "julia",
         "--project=@nvim-lspconfig",
         "-J" .. vim.fn.getenv("HOME") .. "/.julia/environments/nvim-lspconfig/languageserver.so",
+        "-J" .. vim.fn.getenv("HOME") .. "/.julia/environments/nvim-lspconfig/juliaformatter.so",
+        "-J" .. vim.fn.getenv("HOME") .. "/.julia/environments/nvim-lspconfig/revise.so",
         "--sysimage-native-code=yes",
         "--startup-file=no",
         "--history-file=no",
@@ -303,22 +304,18 @@ M.setup = function()
             ls_install_path = joinpath(get(DEPOT_PATH, 1, joinpath(homedir(), ".julia")), "environments", "nvim-lspconfig");
             pushfirst!(LOAD_PATH, ls_install_path);
             using LanguageServer;
+            using JuliaFormatter;
+            using Revise;
             popfirst!(LOAD_PATH);
             depot_path = get(ENV, "JULIA_DEPOT_PATH", "");
 
             project_path = let
                 dirname(something(
-                    ## 1. Finds an explicitly set project (JULIA_PROJECT)
                     Base.load_path_expand((
                         p = get(ENV, "JULIA_PROJECT", nothing);
                         p === nothing ? nothing : isempty(p) ? nothing : p)),
-                    ## 2. Look for a Project.toml file in the current working directory,
-                    ##    or parent directories, with $HOME as an upper boundary
                     Base.current_project(),
-                    ## 3. First entry in the load path
                     get(Base.load_path(), 1, nothing),
-                    ## 4. Fallback to default global environment,
-                    ##    this is more or less unreachable
                     Base.load_path_expand("@v#.#"),))
             end
 
@@ -335,6 +332,7 @@ M.setup = function()
             symbolCacheDownload = false,
             runtimeCompletions = true,
             singleFileSupport = true,
+            useRevise = true,
             lint = {
                 NumThreads = 11,
                 missingrefs = "all",
