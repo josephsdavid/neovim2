e61a544782d21b3b6bf6068c758125b12c9cc859
josephsdavid <josephsd@smu.edu>
2022-07-27 00:30:28 -0500

    test

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_create_augroup("bufcheck", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = "bufcheck",
+    pattern = { "gitcommit", "gitrebase" },
+    command = "startinsert | 1",
+})
