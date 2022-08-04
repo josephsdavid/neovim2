b9d6c6464f86871c9b503d7a8f189afea9642729
josephsdavid <josephsd@smu.edu>
2022-08-04 00:20:26 -0500

    fix

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -20,8 +20,9 @@ M.setbuffer = M.setkey("b")
 M.setters = {opt = M.setopt, global = M.setglobal, buffer = M.setbuffer}
 
 local ctime = os.date ("*t")
-if ctime.hour <= 19 then
+if ctime.hour <= 19 and ctime.hour > 8 then
     M.setopt("background", "light")
+    vim.cmd([[colorscheme zenbones]])
 else
     M.setopt("background", "dark")
     require('doom-one').setup({
