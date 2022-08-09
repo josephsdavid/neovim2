b6195fc6fa6a3cbbf25dfb7525795a4136a09fe3
josephsdavid <david.josephs@beacon.bio>
2022-08-09 20:47:18 +0000

    # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #   (use "git restore --staged <file>..." to unstage) # 	modified:   lua/config/keybinds.lua # 	modified:   lua/core/options.lua # 	modified:   lua/core/plugins.lua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -99,7 +99,43 @@ return packer.startup(function(use)
     }
 
     use({
-        'NTBBloodbath/doom-one.nvim', branch = "rewrite"
+        'NTBBloodbath/doom-one.nvim', branch = "rewrite",
+        setup = function()
+            -- Add color to cursor
+            vim.g.doom_one_cursor_coloring = false
+            -- Set :terminal colors
+            vim.g.doom_one_terminal_colors = true
+            -- Enable italic comments
+            vim.g.doom_one_italic_comments = true
+            -- Enable TS support
+            vim.g.doom_one_enable_treesitter = true
+            -- Color whole diagnostic text or only underline
+            vim.g.doom_one_diagnostics_text_color = false
+            -- Enable transparent background
+            vim.g.doom_one_transparent_background = false
+
+            -- Pumblend transparency
+            vim.g.doom_one_pumblend_enable = false
+            vim.g.doom_one_pumblend_transparency = 20
+
+            -- Plugins integration
+            vim.g.doom_one_plugin_neorg = true
+            vim.g.doom_one_plugin_bufferline = true
+            vim.g.doom_one_plugin_telescope = true
+            vim.g.doom_one_plugin_neogit = true
+            vim.g.doom_one_plugin_nvim_tree = true
+            vim.g.doom_one_plugin_dashboard = true
+            vim.g.doom_one_plugin_startify = true
+            vim.g.doom_one_plugin_whichkey = true
+            vim.g.doom_one_plugin_indent_blankline = true
+            vim.g.doom_one_plugin_vim_illuminate = true
+            vim.g.doom_one_plugin_lspsaga = true
+        end,
+        config = function()
+            vim.cmd [[colorscheme doom-one]]
+        end
+
+
     })
 
     use { "chentoast/marks.nvim",
@@ -236,7 +272,14 @@ return packer.startup(function(use)
                 }
             }
         end }
-use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('neogit').setup {
+                disable_commit_confirmation = true,
+                use_magit_keybindgs = true
+            }
+        end
+    }
 
 
     -- Automatically set up your configuration after cloning packer.nvim
