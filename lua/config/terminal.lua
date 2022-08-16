6e21785557b1d575342ab9563f502dcb316f6de7
josephsdavid <josephsd@smu.edu>
2022-08-16 09:55:11 -0500

    update

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -64,4 +64,27 @@ M.setup = function()
     })
 end
 
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<localleader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
 return M
