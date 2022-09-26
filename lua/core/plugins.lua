ae1c59acff1677c36bfe7cf1086e305b29fef286
josephsdavid <david.josephs@beacon.bio>
2022-09-26 18:48:10 +0000

    venn

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -238,7 +238,7 @@ return packer.startup({ function(use)
     use 'kdheepak/JuliaFormatter.vim'
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use({ "pocco81/truezen.nvim" })
-    use({ "nvim-neorg/neorg", tag = "0.0.12"})
+    use({ "nvim-neorg/neorg", tag = "0.0.12" })
     use({ "nvim-neorg/neorg-telescope" })
     -- here is hunk
     -- hunk 2
@@ -471,11 +471,37 @@ return packer.startup({ function(use)
             vim.api.nvim_set_keymap('n', '<leader>dk', ':lua require("duck").cook()<CR>', { noremap = true })
         end
     }
+    use {
+        "jbyuki/venn.nvim",
+        config = function()
+            -- venn.nvim: enable or disable keymappings
+            function _G.Toggle_venn()
+                local venn_enabled = vim.inspect(vim.b.venn_enabled)
+                if venn_enabled == "nil" then
+                    vim.b.venn_enabled = true
+                    vim.cmd [[setlocal ve=all]]
+                    -- draw a line on HJKL keystokes
+                    vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
+                    vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
+                    vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
+                    vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+                    -- draw a box by pressing "f" with visual selection
+                    vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
+                else
+                    vim.cmd [[setlocal ve=]]
+                    vim.cmd [[mapclear <buffer>]]
+                    vim.b.venn_enabled = nil
+                end
+            end
+            -- toggle keymappings for venn using <leader>v
+            vim.api.nvim_set_keymap('n', '<leader>v', ":lua Toggle_venn()<CR>", { noremap = true })
+        end
+    }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
         require("packer").sync()
     end
 end,
-config = { max_jobs = 25 } }
+    config = { max_jobs = 25 } }
 )
