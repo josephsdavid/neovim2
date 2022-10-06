8c9dc6c3ba307566f1c3583658206be6791f6c26
josephsdavid <josephsd@smu.edu>
2022-10-05 21:11:41 -0500

    smol

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -10,7 +10,7 @@ Bindings.setup(Bindings.config)
 require "config.autocmds"
 require "config.snippets"
 require "config.telescope"
-require "config.norg"
+-- require "config.norg" - called in plugins.lua
 require "config.lines"
 -- require "config.namespaces"
 
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -233,10 +233,12 @@ return packer.startup({ function(use)
     --     "windwp/nvim-autopairs",
     --     config = function() require("nvim-autopairs").setup {} end
     -- }
-    use 'kdheepak/JuliaFormatter.vim'
+    use {'kdheepak/JuliaFormatter.vim', ft="julia"}
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
     use({ "pocco81/truezen.nvim" })
-    use({ "nvim-neorg/neorg" })
+    use({ "nvim-neorg/neorg", ft="norg", config = function ()
+        require("config.norg")
+    end })
     use({ "nvim-neorg/neorg-telescope" })
     -- here is hunk
     -- hunk 2
@@ -413,55 +415,6 @@ return packer.startup({ function(use)
     use { "rafcamlet/tabline-framework.nvim", requires = "kyazdani42/nvim-web-devicons" } -- BROKEN: dev comments
     use "sindrets/diffview.nvim"
     use "rebelot/heirline.nvim"
-    use { 'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup {
-                signs                        = {
-                    add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-                    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn' },
-                    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn' },
-                    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr',
-                        linehl = 'GitSignsDeleteLn' },
-                    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr',
-                        linehl = 'GitSignsChangeLn' },
-                },
-                signcolumn                   = false, -- Toggle with `:Gitsigns toggle_signs`
-                numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
-                linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
-                word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
-                watch_gitdir                 = {
-                    interval = 1000,
-                    follow_files = true
-                },
-                attach_to_untracked          = true,
-                current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-                current_line_blame_opts      = {
-                    virt_text = true,
-                    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-                    delay = 1000,
-                    ignore_whitespace = false,
-                },
-                current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-                sign_priority                = 6,
-                update_debounce              = 100,
-                status_formatter             = nil, -- Use default
-                max_file_length              = 40000, -- Disable if file is longer than this (in lines)
-                preview_config               = {
-                    -- Options passed to nvim_open_win
-                    border = 'single',
-                    style = 'minimal',
-                    relative = 'cursor',
-                    row = 0,
-                    col = 1
-                },
-                yadm                         = {
-                    enable = false
-                },
-            }
-        end
-    }
     use {
         'tamton-aquib/duck.nvim',
         config = function()
