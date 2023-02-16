3eeac7687364024a82bbe476ad758d52ec4350c8
josephsdavid <david.josephs@beacon.bio>
2023-02-16 15:45:10 +0000

    ok

--- ftplugin/julia.lua
+++ ftplugin/julia.lua
@@ -1,4 +1,4 @@
-vim.keymap.set( "n",km.localleader("f"), ":JuliaFormatterFormat<CR>",
+vim.keymap.set( "n",km.localleader("f"), ":Format<CR>",
     {desc =  "Julia formatter format",  noremap = true, silent = false })
 vim.keymap.set( "v",km.localleader("f"), ":JuliaFormatterFormat<CR>",
     {desc =  "Julia formatter format",  noremap = true, silent = false })
